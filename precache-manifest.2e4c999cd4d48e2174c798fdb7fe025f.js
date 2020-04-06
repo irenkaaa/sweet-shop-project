7f9672f49a6e73a0f3ef945490d1c4bad756b276
Irena Mishonova <35763047+irenkaaa@users.noreply.github.com>
2020-04-06 10:55:18 +0300

    Updates

---
+++ precache-manifest.2e4c999cd4d48e2174c798fdb7fe025f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3b9d6e35e93c06d7c323c80f19d872",
+    "url": "/sweet-shop-project/index.html"
+  },
+  {
+    "revision": "21de42f824580859713f",
+    "url": "/sweet-shop-project/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "11fc52bad357fea041d8",
+    "url": "/sweet-shop-project/static/css/main.402bf80a.chunk.css"
+  },
+  {
+    "revision": "21de42f824580859713f",
+    "url": "/sweet-shop-project/static/js/2.5e819bba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sweet-shop-project/static/js/2.5e819bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fc52bad357fea041d8",
+    "url": "/sweet-shop-project/static/js/main.d3cef620.chunk.js"
+  },
+  {
+    "revision": "8d8931e1deabf8a01644",
+    "url": "/sweet-shop-project/static/js/runtime-main.279342db.js"
+  },
+  {
+    "revision": "a12ae61385d6728d25baadae7245103d",
+    "url": "/sweet-shop-project/static/media/screen_shot_2011-05-12_at_11.25.38.a12ae613.png"
+  }
+]);
