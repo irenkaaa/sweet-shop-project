10b14a511429c1a9766bfb7e101dcee566750b95
Irena Mishonova <35763047+irenkaaa@users.noreply.github.com>
2020-05-14 14:51:53 +0300

    Update users.jsx

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -9,20 +9,20 @@ class Users extends Component {
         const { loadUsers } = this.props;
         return (
             <div>
-                <h2>User List:</h2>
+                <h2 style={{ textAlign:'center', margin:'2em 0'}}>User List:</h2>
                 
                     <div>
                         <ul>
                                 {
                                     loadUsers.map( user => (
-                                        <li key={user._id}>
+                                        <li key={user._id} style={{ paddingBottom:'1em'}}>
                                             {user.username} with id: {user._id} 
                                             {
                                                 (user.roles == 'Admin') ? ( null ) : 
                                                 (
-                                                    <span>
+                                                    <span style={{ paddingLeft:'1em'}}>
                                                         <button onClick={(e) => this.props.promoteUser(e, user._id)}>Promote to Admin</button>
-                                                        <button onClick={(e) => this.props.deleteUser(e, user._id)}>Delete User</button>
+                                                        <button onClick={(e) => this.props.deleteUser(e, user._id)} style={{ marginLeft:'1em'}} >Delete User</button>
                                                     </span>
                                                 )
                                             }
