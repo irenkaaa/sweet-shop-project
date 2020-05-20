d34188fd36e156be064bef3c432330f85458b963
Irena Mishonova <35763047+irenkaaa@users.noreply.github.com>
2020-05-20 13:30:15 +0300

    homepage added to package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
